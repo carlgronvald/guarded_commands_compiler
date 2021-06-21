c7e22ffee3844410b551ecfc2e1546c56b11e521
Carl Frederik Grønvald <carl@gronvald.com>
2021-06-21 14:05:59 +0200

    :sparkles: ~The final script.fsx~

--- GuardedCommands/Script.fsx
+++ GuardedCommands/Script.fsx
@@ -1,5 +1,6 @@
 ﻿// Michael R. Hansen 07-06-2021
 
+// Author: Carl Frederik Grønvald 21/06/2021, though many iterations have existed
 
 // You must revise the following path 
 
@@ -29,25 +30,36 @@ open CompilerUtil
 
 System.IO.Directory.SetCurrentDirectory __SOURCE_DIRECTORY__;;
 
-// The Ex0.gc example:
+parseFromFile "chartest.gc"
+tcP (parseFromFile "chartest.gc")
+execOpt "chartest.gc"
 
-Tests.test();;
+List.iter execOpt ["Ex0.gc";"Ex1.gc";"Ex2.gc";"Ex3.gc"]
+List.iter execOpt ["Ex4.gc";"Ex5.gc";"Ex6.gc";"Ex7.gc"]
+goTraceOpt (parseFromFile "Ex7.gc")
+
+List.iter execOpt ["fact.gc"; "factCBV.gc"; "factRec.gc"]
+
+List.iter execOpt ["QuickSortV1.gc"; "QuickSortV2.gc"; "factImpPTyp.gc"]
+execOpt "par1.gc"
+
+execOpt "Test_functions.gc"
+
+execOpt "QuickSortV2.gc"
 
 
 
 (* Testing functions and procedures *)
-(*
+
 let testTree = parseFromFile  "A0.gc";;
 printfn "%A" testTree
 let code = CP testTree
 printfn "%A" code
-*)
 
 
 
 
 
-(*
 let ex0Tree = parseFromFile "Ex0.gc";;
 
 let _ = tcP ex0Tree;;
@@ -82,16 +94,14 @@ let _ = exec "Ex1.gc";;
 
 let _ = exec "Ex2.gc";;
 
-// Test of programs covered by the fifth task using optimized compilation (Section 8.2):
-List.iter execOpt ["Ex1.gc"; "Ex2.gc"];;
 
 // All programs relating to the basic version can be parsed:
 let pts = List.map parseFromFile ["Ex1.gc"; "Ex2.gc";"Ex3.gc"; "Ex4.gc"; "Ex5.gc"; "Ex6.gc"; "Skip.gc"];;
 
 // The parse tree for Ex3.gc
 List.item 2 pts ;;
-*)
-(*
+
+
 // Test of programs covered by the first task (Section 3.7):
 List.iter exec ["Ex1.gc"; "Ex2.gc";"Ex3.gc"; "Ex4.gc"; "Ex5.gc"; "Ex6.gc"; "Skip.gc"];;
 
@@ -99,15 +109,34 @@ List.iter exec ["Ex1.gc"; "Ex2.gc";"Ex3.gc"; "Ex4.gc"; "Ex5.gc"; "Ex6.gc"; "Skip
 List.iter exec ["Ex7.gc"; "fact.gc"; "factRec.gc"; "factCBV.gc"];;
 
 // Test of programs covered by the fourth task (Section 5.4):
+goTrace (parseFromFile "A0.gc")
 List.iter exec ["A0.gc"; "A1.gc"; "A2.gc"; "A3.gc"];;
 
 // Test of programs covered by the fifth task (Section 6.1):
-List.iter exec ["A4.gc"; "Swap.gc"; "QuickSortV1.gc"];;
+List.iter exec ["Swap.gc"; "QuickSortV1.gc"];;
 
 // Test of programs covered by the fifth task (Section 7.4):
-List.iter exec ["par1.gc"; "factImpPTyp.gc"; "QuickSortV2.gc"; "par2.gc"];;
+List.iter exec ["par1.gc"; "factImpPTyp.gc"; "QuickSortV2.gc"];;
 
 // Test of programs covered by the fifth task using optimized compilation (Section 8.2):
-List.iter execOpt ["par1.gc"; "factImpPTyp.gc"; "QuickSortV2.gc"; "par2.gc"];;
-*)
+List.iter execOpt ["par1.gc"; "factImpPTyp.gc"; "QuickSortV2.gc"];;
+
+// Our own tests
+List.iter execOpt ["chartest.gc"; "function_test.gc"; "function_test_2.gc"; "Test_functions.gc"; "Ex11.gc"; "Exa1.gc"];;
+
+// More of our tests
+List.iter execOpt ["arr_test.gc"; "cond_test.gc"; "div_test.gc"; "do_od_test.gc"; "ge_test.gc"; "if_fi_test.gc"; "mass_test.gc"];;
+
+// Time unoptimized and optimized code
+let comp_time filename =
+    let total_opt_time = seq {for i in 1..10 do execOptT filename} |> Seq.sum
+    let total_unopt_time = seq {for i in 1..10 do execT filename} |> Seq.sum
+    
+    printfn "Total opt time %A, total unopt time %A" total_opt_time total_unopt_time
+    
+comp_time "QuickSortV2.gc"
+comp_time "QuickSortV1.gc"
+comp_time "Ex11_no_output.gc"
+comp_time "Ex7.gc"
+
 
